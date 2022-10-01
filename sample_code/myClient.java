c312ee9372847f4b5f496e2824798f326db5df56
marcoalexander <timmythetom132@gmail.com>
2022-10-01 17:24:00 -0500

    finished logic of PA1, now revise code to make pretty

--- sample_code/myClient.java
+++ sample_code/myClient.java
@@ -1,4 +1,11 @@
 /**
+ REVISED EDITION:
+  By Marco Tabora
+  Course: CSCI 4311
+  Using the source code with description below, this is a client-server environment in which this client part
+  will send a user inputed file name to server, server will check if file name exists and send the file, client will
+  receive the file, save it under new name. If file does not exist error message will be sent from server to client.
+ ______________________________________________________
 By Dr Md Tamjidul Hoque, University of New Orleans.
 For course: CSCI-4311/5311.
 In a client-server Environment, this is the client part, sending a number (e.g., 6) to the server to receive the square of that sent number.
@@ -13,32 +20,96 @@ public class myClient{
 
    static PrintStream ps = null;     // to send message
    static Scanner sis = null;        // to receive message
-
-   public static void send(int myint) throws IOException
-   { ps.println (myint); // send message
-     ps.flush(); // flush the message
-     System.out.println("I (Client) sent the Server: " + myint);
+   /*
+   public static void send() throws IOException
+   { 
    }
-
-   public static void receive() throws IOException
-   { int value= sis.nextInt();
-     System.out.println("Server sent me (Client): " + value);
+*/
+   public static int receive() throws IOException
+   { 
+    int value = sis.nextInt();
+    return value;
    }
-
+   
    public static void main(String args[])
    {
+    //create scanner to take input(file name) from user.
+    //send file name to server
+    Scanner input = new Scanner(System.in);
+    System.out.println("Please enter file you want to pull from server, in <FileName>.<FileExtension> format: ");
+    File fileToSend = new File(input.nextLine());//new code
+
       Socket clt_skt = null;
       try {
+
+              
+
+
+            //client opening
 	          clt_skt = new Socket("127.0.0.1", 1357); // "127.0.0.1" is "localhost"; so you can also try "localhost"
-	          ps = new PrintStream(clt_skt.getOutputStream());
+
+              ps = new PrintStream(clt_skt.getOutputStream());
               sis = new Scanner(clt_skt.getInputStream());
+              //sending code
+              //FileInputStream fileInputStream = new FileInputStream(fileToSend.getAbsolutePath());
+              DataOutputStream dataOutputStream = new DataOutputStream(clt_skt.getOutputStream());
+
+              String fileName = fileToSend.getName();//String of file name
+              byte[] fileNameBytes = fileName.getBytes(); //String of file name converted to byte[]
+
+              //byte[] fileContentBytes = new byte[(int) fileToSend.length()]; //file content in byte[]
+              //fileInputStream.read(fileContentBytes); //reading file content bytes, puts data into byte[]
+              //sending file name to server
+              dataOutputStream.writeInt(fileNameBytes.length);
+              dataOutputStream.write(fileNameBytes);
+
+              //receive input from server
+              String fileNameDownload = "";
+
+
+
+              //if file exists, download file with next name
+            if (receive() != -1) {
+                    DataInputStream dataInputStream = new DataInputStream(clt_skt.getInputStream());
+                    int fileNameLength = dataInputStream.readInt();
+            
+             if (fileNameLength > 0) {
+                byte[] fileNameBytesDownload = new byte[fileNameLength];
+                dataInputStream.readFully(fileNameBytesDownload, 0, fileNameBytesDownload.length);
+                fileNameDownload = new String(fileNameBytesDownload);
+              }
+
+
+
+             int fileContentLength = dataInputStream.readInt();
+
+              if (fileContentLength > 0) {
+                byte[] fileContentBytes = new byte[fileContentLength];
+
+                dataInputStream.readFully(fileContentBytes, 0 ,fileContentLength);
+
+                File fileToDownload = new File("clt_" + fileNameDownload);
+                FileOutputStream fileOutputStream = new FileOutputStream(fileToDownload);
+                fileOutputStream.write(fileContentBytes);
+                fileOutputStream.close();
+              }
+            } else {
+                String errorMsg = sis.nextLine();
+                System.out.println(errorMsg);
+              }
+              //else print error message to user
+
+              //dataOutputStream.writeInt(fileContentBytes.length);
+              //dataOutputStream.write(fileContentBytes);
+              //end of sending code
+
    }
        catch (IOException e)
 	    {System.out.println(" Error connecting: " + e); }
        try
-        {send(6); // Sending a value to the server to get square of that value for example
-         receive();
-         clt_skt.close();
+        {
+          //client closing
+          clt_skt.close();
         }
        catch (IOException e)
          {System.out.println(" Error sending:" + e);}
@@ -48,4 +119,24 @@ public class myClient{
 
 }
 
+/*
+sending code:
 
+//sending code
+              FileInputStream fileInputStream = new FileInputStream(fileToSend.getAbsolutePath());
+              DataOutputStream dataOutputStream = new DataOutputStream(clt_skt.getOutputStream());
+
+              String fileName = fileToSend.getName();//String of file name
+              byte[] fileNameBytes = fileName.getBytes(); //String of file name converted to byte[]
+
+              byte[] fileContentBytes = new byte[(int) fileToSend.length()]; //file content in byte[]
+              fileInputStream.read(fileContentBytes); //reading file content bytes, puts data into byte[]
+
+              dataOutputStream.writeInt(fileNameBytes.length);
+              dataOutputStream.write(fileNameBytes);
+
+              dataOutputStream.writeInt(fileContentBytes.length);
+              dataOutputStream.write(fileContentBytes);
+              //end of sending code
+
+*/
--- sample_code/myServer.java
+++ sample_code/myServer.java
@@ -1,9 +1,18 @@
 /**
+ REVISED EDITION:
+  By Marco Tabora
+  Course: CSCI 4311
+  Using the source code with description below, this is a client-server environment in which this server part
+  will receive input file name from client, check if file exists in server directory if it exists it sends it, else
+  send error message to client.
+ ______________________________________________________
+
 By Dr Md Tamjidul Hoque, University of New Orleans.
 For course: CSCI-4311/5311.
 In a client-server Environment, this is the server part, receiving a number (e.g., 6) and then sending the square of that to the client.
 */
 
+//importing needed libraries
 import java.io.*;
 import java.net.*;
 import java.util.Scanner;
@@ -13,40 +22,84 @@ public class myServer{
 
    static PrintStream ps = null;     // to send message
    static Scanner sis = null;        // to receive message
-   static int ComputedValue =0;
 
-   public static void send(int myint) throws IOException
-   { ps.println (myint); // send message
+  // possible method creation to simplify code
+   public static void send(String fileName) throws IOException
+   {
+     ps.println (-1);
+     ps.println ("The file " + fileName + " is not available."); // send message
      ps.flush(); // flush the message
-     System.out.println("I (Server) sent the Client: " + myint);
+    
    }
-
+/*
    public static void receive() throws IOException
-   { int value= sis.nextInt();
-     System.out.println("I (Server) received: " + value);
-     ComputedValue = value * value;
+   { 
    }
-
+    */
    public static void main(String args[])
    {
+      //server opening
       ServerSocket svr_skt = null;
       Socket skt = null;
+      String fileName = "";
       try {
+              
+
+
 	          svr_skt = new ServerSocket(1357); // "Server provides services via port - so port number is needed"
 	          skt = svr_skt.accept();
-	          sis = new Scanner(skt.getInputStream());
-	          ps = new PrintStream(skt.getOutputStream());
+
+              ps = new PrintStream(skt.getOutputStream());
+              sis = new Scanner(skt.getInputStream());
+            //receiving code
+            DataInputStream dataInputStream = new DataInputStream(skt.getInputStream());
+            int fileNameLength = dataInputStream.readInt();
+
+            if (fileNameLength > 0) {
+              ps.println (1);
+              byte[] fileNameBytes = new byte[fileNameLength];
+              dataInputStream.readFully(fileNameBytes, 0, fileNameBytes.length);
+              fileName = new String(fileNameBytes);
+
+              //creating file name and checking if it exists.
+              //if so, send file.
+              File fileToSend = new File(fileName);
+
+            if (fileToSend.exists()) {
+                //send file
+                FileInputStream fileInputStream = new FileInputStream(fileToSend.getAbsolutePath());
+                DataOutputStream dataOutputStream = new DataOutputStream(skt.getOutputStream());
+
+                String fileName1 = fileToSend.getName();//String of file name
+                byte[] fileNameBytesSend = fileName1.getBytes(); //String of file name converted to byte[]
+
+                byte[] fileContentBytes = new byte[(int) fileToSend.length()]; //file content in byte[]
+                fileInputStream.read(fileContentBytes); //reading file content bytes, puts data into byte[]
+
+                dataOutputStream.writeInt(fileNameBytesSend.length);
+                dataOutputStream.write(fileNameBytesSend);
+
+                dataOutputStream.writeInt(fileContentBytes.length);
+                dataOutputStream.write(fileContentBytes);
+                //end of sending code
+
+              }else {
+                //send error message to user
+                send(fileName);
+              }
+            
+            }
         }
        catch (IOException e)
 	    {System.out.println(" Error connecting: " + e); }
        try
-        {receive(); // Sending a value to the server to get square of that value, for example
-         send(ComputedValue);
+        { 
+          //server closing
          skt.close();
          svr_skt.close();
         }
        catch (IOException e)
-         {System.out.println(" Error sending:" + e);}
+         {System.out.println(" Error creating/downloading file:" + e);}
 
         System.out.println("Server side communication ended successfully!");
    }
@@ -54,3 +107,30 @@ public class myServer{
 }
 
 
+/*
+receiving code:
+
+            DataInputStream dataInputStream = new DataInputStream(clt_skt.getInputStream());
+            int fileNameLength = dataInputStream.readInt();
+            
+            if (fileNameLength > 0) {
+              byte[] fileNameBytes = new byte[fileNameLength];
+              dataInputStream.readFully(fileNameBytes, 0, fileNameBytes.length);
+              String fileName = new String(fileNameBytes);
+              }
+
+
+
+            int fileContentLength = dataInputStream.readInt();
+
+              if (fileContentLength > 0) {
+                byte[] fileContentBytes = new byte[fileContentLength];
+
+                dataInputStream.readFully(fileContentBytes, 0 ,fileContentLength);
+
+                File fileToDownload = new File("clt_" + fileName);
+                FileOutputStream fileOutputStream = new FileOutputStream(fileToDownload);
+                fileOutputStream.write(fileContentBytes);
+                fileOutputStream.close();
+
+*/
