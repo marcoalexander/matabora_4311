e8cda8b3b363a2e4b4915b3a4ec3d4fa54acca04
marcoalexander <timmythetom132@gmail.com>
2022-11-03 13:10:53 -0500

    adding PA documentation and starter code

---
+++ PA2/Tree.java
@@ -0,0 +1,40 @@
+import java.util.*;
+import java.text.NumberFormat;
+import java.text.DecimalFormat;
+
+public class Tree {
+    public static void main(String[] args) {
+
+        //Building First Tree
+        Vertex a1 = new Vertex("1");
+        Vertex b2 = new Vertex("2");
+        Vertex c3 = new Vertex("3");
+        Vertex d4 = new Vertex("4");
+        /*
+        Vertex e1 = new Vertex("e");
+        Vertex f1 = new Vertex("f");
+        Vertex g1 = new Vertex("g");
+        Vertex h1 = new Vertex("h");
+        */
+        a1.addNeighbor(c1,0);
+        c1.addNeighbor(b1,0);
+        c1.addNeighbor(d1,0);
+        d1.addNeighbor(b1,0);
+        /*
+        b1.addNeighbor(h1,0);
+        b1.addNeighbor(d1,0);
+        d1.addNeighbor(f1,0);
+        */
+        //BFS
+        BreadthFirstSearch bfs=new BreadthFirstSearch();
+        long start1 = System.nanoTime();
+        bfs.traverse(a1); 
+        long end1 = System.nanoTime();
+        
+        NumberFormat formatter = new DecimalFormat("#0.0000");
+        System.out.print("Execution time is " + formatter.format((end1 - start1) / 1000000d) +" "+ " milliseconds");
+
+        System.out.println();
+        
+    }
+}
---
+++ PA2/Vertex.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+public class Vertex {
+    final private String name;
+
+    private boolean visited;
+    private Map<Vertex,Integer> neighbors;
+    //added later
+    private Integer distance = Integer.MAX_VALUE;
+    //private LinkedList<Vertex> shortestPath = new LinkedList<>();
+    //
+
+    public Vertex(String name) {
+        this.name = name;
+        this.neighbors = new HashMap<Vertex,Integer>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public boolean isVisited() {
+        return visited;
+    }
+
+    public void setVisited(boolean visited){
+        this.visited = visited;
+    }
+
+    public Map<Vertex,Integer> getNeighbors(){
+        return neighbors;
+    }
+
+    public void addNeighbor(Vertex vertex,int weight){
+        neighbors.put(vertex,weight);
+        //this.neighbors.add(map);
+
+    }
+
+}
